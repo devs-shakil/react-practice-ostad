b438ce14d03dbd5dc1be1a545d55b9d2fa13575f
shakilwebmaster <mdshakil102036@gmail.com>
2023-07-10 11:58:37 +0600

    react practice

--- vite-project/src/component/Header.jsx
+++ vite-project/src/component/Header.jsx
@@ -1,7 +1,28 @@
+
 const Header = () => {
+    let marks = 75;
     return (
+        // immidietly invocked function
         <div>
-            <h2>Hello Bangladesh</h2>
+            {(()=>{ 
+                if(marks >= 80 && marks <= 100){
+                    return "A+"
+                }else if(marks >= 70 && marks <80){
+                    return "A"
+                }else if(marks >= 60 && marks <70){
+                    return "A-"
+                }else if(marks >= 50 && marks <60){
+                    return "B"
+                }else if(marks >= 40 && marks <50){
+                    return "C"
+                }else if(marks >= 33 && marks <40){
+                    return "D"
+                }else if(marks < 33){
+                    return "F"
+                }
+
+
+            })()}            
         </div>
     );
 };
