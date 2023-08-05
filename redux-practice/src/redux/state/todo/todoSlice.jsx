28eab0f70186a1607133457fe375564df3821d52
shakilwebmaster <mdshakil102036@gmail.com>
2023-08-05 13:26:08 +0600

    edit button add

--- redux-practice/src/redux/state/todo/todoSlice.jsx
+++ redux-practice/src/redux/state/todo/todoSlice.jsx
@@ -1,29 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 export const todoSlice = createSlice({
-  name: "todo",
-  initialState: {
-    value: [],
-  },
-  reducers: {
-    AddTodo: (state, action) => {
-      state.value.push(action.payload);
+    name:"todo",
+    initialState:{
+        value:[]
     },
-    RemoveTodo: (state, action) => {
-      state.value.splice(action.payload, 1);
-    },
-    EditTodo: (state, action) => {
-      const { index, task } = action.payload;
-      state.value[index].task = task; // Update 'task' property instead of using splice
-    },
-    ToggleCompleted: (state, action) => {
-      const index = action.payload;
-      if (state.value[index]) {
-        state.value[index].completed = !state.value[index].completed;
-      }
-    },
-  },
-});
+    reducers:{
+       
+        AddTodo:(state, action) =>{
+           
+            state.value.push(action.payload)
+        },
+        RemoveTodo:(state, action) =>{
+            state.value.splice(action.payload, 1)
+        },
+        EditTodo:(state, action) =>{
+            state.value.splice(action.payload['index'], 1, action.payload['task'])
+        },
+        ToggleCompleted:(state, action) =>{
+            const index = action.payload;
+            state.value[index].completed = !state.value[index].completed;
+        }
+    }
+})
 
-export const { AddTodo, RemoveTodo, EditTodo, ToggleCompleted } = todoSlice.actions;
+export const {AddTodo, RemoveTodo, EditTodo, ToggleCompleted} = todoSlice.actions;
 export default todoSlice.reducer;
